b0d9ecffbc87a0e5cb38667ad0f03d0a5deacbaa
Swiftton <swiftton@gmail.com>
2017-04-11 12:08:47 +0300

    Initial commit

---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+//Example of middleware
+app.use((request, response, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${request.method} ${request.url}`;
+  fs.appendFile('server.log', log + '\n', (error) => {
+    if (error) console.log('Unable to store logs to server.log');
+  });
+
+  console.log(log);
+  next();
+});
+
+/*
+app.use((request, response, next) => {
+  response.render('information.hbs');
+});
+*/
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('convertToUpper', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (request, response) => {
+  response.render('home.hbs', {
+    pageTitle: 'Home Page Title',
+    welcomeMessage: 'Hello, user!'
+  });
+});
+
+app.get('/about', (request, response) => {
+  response.render('about.hbs', {
+    pageTitle: 'About Page Title'
+  });
+});
+
+app.get('/bad', (request, response) => {
+  response.send({
+    errorMessage: 'Something wrong'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on the port 3000');
+});
